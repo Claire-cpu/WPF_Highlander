2231be91d69275a5c5ae64b1c8e08a64422873d4
carazhang93 <133140681+carazhang93@users.noreply.github.com>
2024-12-02 18:18:26 -0500

    Add project files.

---
+++ ClassComponents_Highlander/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassComponents_Highlander
+{
+    public class Class1
+    {
+    }
+}
