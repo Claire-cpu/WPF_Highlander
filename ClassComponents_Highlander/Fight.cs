9a3d19ead0aa557a60cf5ca05a1becee5c8d10cc
Cort Reynolds-Bolan <reynoldsbolan.c@gmail.com>
2024-12-13 13:01:44 -0500

    Implemented queries to update Highlanders table at the end of the game for option1.

--- ClassComponents_Highlander/Fight.cs
+++ ClassComponents_Highlander/Fight.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
 namespace ConsoleApp_HighLander
 {
@@ -22,29 +23,31 @@ namespace ConsoleApp_HighLander
             //Handle ties
             if (self.PowerLevel == opponent.PowerLevel)
             {
-                HandleTie(self, opponent);
+                HandleTie(self, opponent, app);
             }
             else
             {
-                ResolveFight(self, opponent);
+                ResolveFight(self, opponent, app);
             }
         }
 
-        private void HandleTie(Highlander self, Highlander opponent)
+        private void HandleTie(Highlander self, Highlander opponent, ConsoleApp app)
         {
             string message;
-
+            
             if (self.Age > opponent.Age)
             {
                 opponent.IsAlive = false;
                 self.PowerLevel += opponent.PowerLevel;
                 message = $"{self.Name} wins by age advantage against {opponent.Name} and absorbs {opponent.PowerLevel} power!";
+                app.IncrementVictimCount(self.Name);
             }
             else if (self.Age < opponent.Age)
             {
                 self.IsAlive = false;
                 opponent.PowerLevel += self.PowerLevel;
                 message = $"{opponent.Name} wins by age advantage against {self.Name} and absorbs {self.PowerLevel} power!";
+                app.IncrementVictimCount(opponent.Name);
             }
             else
             {
@@ -55,19 +58,21 @@ namespace ConsoleApp_HighLander
                     opponent.IsAlive = false;
                     self.PowerLevel += opponent.PowerLevel;
                     message = $"{self.Name} wins by random decision!";
+                    app.IncrementVictimCount(self.Name);
                 }
                 else
                 {
                     self.IsAlive = false;
                     opponent.PowerLevel += self.PowerLevel;
                     message = $"{opponent.Name} wins by random decision!";
+                    app.IncrementVictimCount(opponent.Name);
                 }
             }
             Console.Write(message);
             Logger.Log(message);
         }
 
-        private void ResolveFight(Highlander self, Highlander opponent)
+        private void ResolveFight(Highlander self, Highlander opponent, ConsoleApp app)
         {
             string message;
             //Find stronger highlander
@@ -80,12 +85,14 @@ namespace ConsoleApp_HighLander
                 self.PowerLevel += opponent.PowerLevel;
                 opponent.IsAlive = false;
                 message = $"{self.Name} wins against {opponent.Name} and absorbs {opponent.PowerLevel} power!";
+                app.IncrementVictimCount(self.Name);
             }
             else
             {
                 opponent.PowerLevel += self.PowerLevel;
                 self.IsAlive = false;
                 message = $"{opponent.Name} wins against {self.Name} and absorbs {self.PowerLevel} power!";
+                app.IncrementVictimCount(opponent.Name);
             }
 
             Console.WriteLine(message);
